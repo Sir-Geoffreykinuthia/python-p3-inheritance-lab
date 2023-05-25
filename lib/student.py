6b40f8c12509b707fb69ffc366686d480cca9369
geoffrey kinuthia <geoffreykinuthia10@gmail.com>
2023-05-25 06:37:18 +0300

    Done

--- lib/student.py
+++ lib/student.py
@@ -2,7 +2,14 @@
 
 from user import User
 
+#  The Student class inherits from the User class and has an empty 'knowledge' list.
+#     The 'learn' method appends information to the 'knowledge' list.
+
 class Student(User):
+    def __init__(self, first_name, last_name):
+        super().__init__(first_name, last_name)
+        self.knowledge = []
+
     
-    def learn(self):
-        pass
+    def learn(self, information):
+        self.knowledge.append(information)
